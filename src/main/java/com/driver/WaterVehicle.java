b5f017d9af770ca18c7b2c785fbc75bfd3847868
rachu <rachutireshwanth@gmail.com>
2022-12-18 02:02:00 +0530

    ok

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,3 +4,25 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
+
+class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+    public String getVehicleName(){
+        return name;
+    }
+}
+
