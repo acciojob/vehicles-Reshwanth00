d561b7ef26557ecf2aeb9a8d072c6290c98cae5f
Reshwanth00 <116064259+Reshwanth00@users.noreply.github.com>
2022-12-17 19:15:11 +0530

    Create Boat.java

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,22 @@
+package com.driver;
+public class Boat implements WaterVehicle{
+    String name;
+    int capacity;
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public void setCapacity(int capacity){
+        this.capacity = capacity;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+    public String getVehicleName(){
+        return name;
+    }
+}
+
