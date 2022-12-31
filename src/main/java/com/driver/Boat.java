a899e09117af5c6f48d50d179cf77f77ca9e2244
resh <rachutireshwanth@gmail.com>
2022-12-31 15:54:06 +0530

    done

---
+++ src/main/java/com/driver/Boat.java
@@ -0,0 +1,16 @@
+package com.driver;
+class Boat implements WaterVehicle{
+    public String name;
+    public int capacity;
+    public Boat(String name,int capacity){
+        this.name = name;
+        this.capacity = capacity;
+    }
+    public int getVehicleCapacity() {
+        return capacity;
+    }
+    public String getVehicleName(){
+        return name;
+    }
+}
+
--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -4,19 +4,3 @@ public interface WaterVehicle {
     public String getVehicleName();
     public int getVehicleCapacity();
 }
-
-class Boat implements WaterVehicle{
-    public String name;
-    public int capacity;
-    public Boat(String name,int capacity){
-        this.name = name;
-        this.capacity = capacity;
-    }
-    public int getVehicleCapacity() {
-        return capacity;
-    }
-    public String getVehicleName(){
-        return name;
-    }
-}
-
